97ff518a5762a60252d38c80034347eb6694065a
ahpicl64 <ahpicl64@gmail.com>
2025-03-12 14:45:45 +0900

    fix(challenge_api): db 컬렉션 호출 관련 변수 추가/변경 및 단일화

--- apis/challenge_api.py
+++ apis/challenge_api.py
@@ -7,6 +7,8 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client.jungle7
 user_collection = db.user
 challenges = db.challenges  # 챌린지 컬렉션
+participants_list = challenges.participants
+my_id = request.cookies.get("user_id")
 challenge_routes = Blueprint("challenge", __name__)  # 블루프린트 생성
 
 
@@ -23,7 +25,7 @@ def generate_challenge():
 
     duration = get_duration(start_date_receive, end_date_receive)
 
-    host_user_id = request.cookies.get("user_id")
+    host_user_id = my_id
 
     # 챌린지 추최자의 정보 획득
     participant_data = find_user_data(host_user_id)
@@ -68,16 +70,19 @@ def challenge_list():
 # 접속된 url에 있는 값(challenge/~id)을 변수로 받아 함수 인자로 전달
 def detail(id):
     # challenges 내의 "_id"중 받아온 id와 일치하는지 확인(이때 ObjectId로 형변환)
-    challenge = db.challenges.find_one({"_id": ObjectId(id)})
+    challenge = challenges.find_one({"_id": ObjectId(id)})
 
     # id가 일치하는 challenge를 detail.html로 render해서 challenge로 보냄
     return render_template("chal_detail.html", challenge=challenge)
 
+# 참가자 목록과 사용자 id를 대조하여 챌린지 참가여부 검증 기능
+# def check_participation(user_id)
+#     challenges.participants_list.find_one({"participant_id": })
 
 # 유저 id 참조, 데이터(이름, 프로필 사진) 조회하는 기능
 def find_user_data(id):
     # user collection 내 일치하는 id 조회, 해당되는 id의 이름, 프로필 사진 가져오기
-    user = db.users.find_one({"_id": ObjectId(id)}, {"name": 1, "profile_image": 1})
+    user = user_collection.find_one({"_id": ObjectId(id)}, {"name": 1, "profile_image": 1})
 
     # 참가자 정보 결합
     participant_data = {
@@ -98,12 +103,11 @@ def find_user_data(id):
 def join_challenge():
     data = request.json
 
-    # 본문의 챌린지 id / 쿠키의 user id 가져오기
+    # 본문의 챌린지 id
     challenge_id = data.get("challenge_id")
-    user_id = request.cookies.get("user_id")
 
     # 사용자 정보 조회, 데이터 생성
-    participant_data = find_user_data(user_id)
+    participant_data = find_user_data(my_id)
 
     # challenge 컬렉션에 데이터 push
     result = db.challenge.update_one(
